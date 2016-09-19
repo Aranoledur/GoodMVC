bcf4262f8cdbb00c1840f2728cd1581a26d4e852
Nikolay Ischuk <ischuknn@gmail.com>
2016-09-19 18:09:01 +0300

    Profile header and content mvc

---
+++ GoodMVC/Code/UserStories/Profile/Content/ProfileContentController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileContentController.swift
+//  GoodMVC
+//
+//  Created by Nikolay Ischuk on 19.09.16.
+//  Copyright © 2016 Nikolay Ischuk. All rights reserved.
+//
+
+import UIKit
+
+class ProfileContentController: Controller {
+    
+}
---
+++ GoodMVC/Code/UserStories/Profile/Header/ProfileHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileHeaderView.swift
+//  GoodMVC
+//
+//  Created by Nikolay Ischuk on 19.09.16.
+//  Copyright © 2016 Nikolay Ischuk. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: MVCView {
+
+
+}
